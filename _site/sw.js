b674f17101790992f84fbcb65b2ecdeade1c9758
1221jyp <1221jyp@gmail.com>
2023-05-06 23:43:20 +0900

    google search console

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230506.214844';
+const cacheName = 'chirpy-20230506.233941';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
