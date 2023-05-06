664e1349c96d6bb1787fa9e046f488b0e42fe934
1221jyp <98996860+1221jyp@users.noreply.github.com>
2023-05-07 03:39:11 +0900

    6

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230507.032308';
+const cacheName = 'chirpy-20230507.033903';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
