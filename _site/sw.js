2b683346faafb07efd6fd4933b63e7bc97bad9db
1221jyp <1221jyp@gmail.com>
2023-05-06 21:38:03 +0900

    add comments

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230506.032627';
+const cacheName = 'chirpy-20230506.213738';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
